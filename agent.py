3be6ae609d002739f326153357eb0f28fbba4c97
kphung <tomtom7852gphung@gmail.com>
2026-06-25 21:26:26 +0800

    ttttttttt

--- agent.py
+++ agent.py
@@ -57,7 +57,7 @@ tools_desc = [
 
 
 
-# Agent nfdnfjkdadsafdsa
+# Agent nfdnfjkdadsafdsadfsdfd
 async def run_autonomous_agent(email_content, session, llm):
     llm_with_tools = llm.bind_tools(tools_desc , parallel_tool_calls = False)
     
